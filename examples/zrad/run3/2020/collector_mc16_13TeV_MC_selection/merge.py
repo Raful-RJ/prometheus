2f056130b742573cb3007a307607a5214eb1d1f0
Raful-RJ <rafael.rv2012@gmail.com>
2021-02-11 20:41:22 -0300

    Mexi no merge.py do zrad

--- examples/zrad/run3/2020/collector_mc16_13TeV_MC_selection/merge.py
+++ examples/zrad/run3/2020/collector_mc16_13TeV_MC_selection/merge.py
@@ -8,6 +8,7 @@ cdata  = CreateData(10)
 cdata( '~/prometheus/scripts/photon/run3/collector_mc16_13TeV_MC_selection/mc16_13TeV_photons_mcProbes/', '~/prometheus/scripts/photon/run3/collector_mc16_13TeV_MC_selection/mc16_13TeV_photons_mcFakes/', '~/datasets/trigger/photons/npz/mc16_13TeV.sgn.probes_MC_DP.bkg.noMC.JF17' )
 #cdata( 'EGAM2_17/', 'EGAM7_17/', 'data17_13TeV.sgn.probes_lhmedium_Jpsi.bkg.EGAM7' )
 #cdata( 'EGAM2_18/', 'EGAM7_18/', 'data18_13TeV.sgn.probes_lhmedium_Jpsi.bkg.EGAM7' )
+print("Qualquer coisa")
 
 
 
